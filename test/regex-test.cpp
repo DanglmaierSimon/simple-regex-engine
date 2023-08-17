bf0a70f80a47b2be9fde3e7723aebd457bb173cd
Simon Danglmaier <simon.danglmaier@adbsafegate.com>
2023-08-17 08:10:22 +0200

    add negative test cases

--- test/regex-test.cpp
+++ test/regex-test.cpp
@@ -1,12 +1,11 @@
-#include "gtest/gtest.h"
 #include <gtest/gtest.h>
 
 #include "regexp.h"
 
-class RegexEnd2EndTest
+class PositiveTests
     : public testing::TestWithParam<std::pair<std::string, std::string>> {};
 
-TEST_P(RegexEnd2EndTest, PositiveSearches) {
+TEST_P(PositiveTests, PositiveSearches) {
 
   auto params = GetParam();
   auto matcher = create_matcher(params.first);
@@ -15,7 +14,7 @@ TEST_P(RegexEnd2EndTest, PositiveSearches) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    BasicPositiveMatches, RegexEnd2EndTest,
+    BasicPositiveMatches, PositiveTests,
     testing::Values(std::pair<std::string, std::string>{"", ""},
 
                     std::pair<std::string, std::string>{"a*", "a"},
@@ -32,9 +31,44 @@ INSTANTIATE_TEST_SUITE_P(
                     std::pair<std::string, std::string>{"a*b", "ab"},
                     std::pair<std::string, std::string>{"a*b", "aaaaaaaaab"},
                     std::pair<std::string, std::string>{"a*b", "aab"},
+                    std::pair<std::string, std::string>{"a*b", "b"},
 
                     std::pair<std::string, std::string>{"a|b", "a"},
                     std::pair<std::string, std::string>{"a|b", "b"},
 
                     std::pair<std::string, std::string>{"abcd|efgh", "abcd"},
                     std::pair<std::string, std::string>{"abcd|efgh", "efgh"}));
+
+class NegativeTests
+    : public testing::TestWithParam<std::pair<std::string, std::string>> {};
+
+TEST_P(NegativeTests, NegativeSearches) {
+
+  auto params = GetParam();
+  auto matcher = create_matcher(params.first);
+
+  EXPECT_FALSE(matcher(params.second));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    BasicNegativeSearches, NegativeTests,
+    testing::Values(
+        std::pair<std::string, std::string>{"", "a"},
+
+        std::pair<std::string, std::string>{"a*", "ab"},
+        std::pair<std::string, std::string>{"a*", "aaaaavaaaaaaa"},
+
+        std::pair<std::string, std::string>{"a?", "aaa"},
+        std::pair<std::string, std::string>{"a?", "aa"},
+
+        std::pair<std::string, std::string>{"a+", ""},
+
+        std::pair<std::string, std::string>{"a*b", "bab"},
+        std::pair<std::string, std::string>{"a*b", "bbbbb"},
+        std::pair<std::string, std::string>{"a*b", "aaaaaaaaa"},
+
+        std::pair<std::string, std::string>{"a|b", "c"},
+        std::pair<std::string, std::string>{"a|b", "ab"},
+
+        std::pair<std::string, std::string>{"abcd|efgh", "abcdefgh"},
+        std::pair<std::string, std::string>{"abcd|efgh", "efghabcd"}));
